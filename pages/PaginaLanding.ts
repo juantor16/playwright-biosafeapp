88c87a75d0402768afa445c6e8af35e224312ba3
Juan Torres <juantor16@gmail.com>
2025-02-06 15:16:41 -0300

    BPE-8

--- pages/PaginaLanding.ts
+++ pages/PaginaLanding.ts
@@ -1,5 +1,7 @@
 import { Page, Locator, expect } from "@playwright/test" // Importamos Page y Locator de Playwright
+import dotenv from 'dotenv';
 
+dotenv.config();
 export default class PaginaLanding {
     readonly page: Page;
     readonly botonDeRegistrarse: Locator;
@@ -11,6 +13,6 @@ export default class PaginaLanding {
 
     async irARegristroDeCuenta() {
         this.botonDeRegistrarse.click({ force: true });
-        expect(this.page).toHaveURL('https://qa.biosafeapp.com/signup')
+        expect(this.page).toHaveURL(process.env.BASE_URL! + '/signup')
     }
 }
